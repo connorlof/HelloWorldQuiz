fe22a437714171628d8bea36cf70711db7b461c7
Connor Loftus <connorlof@gmail.com>
2019-02-24 10:40:02 -0500

    Created login and sign up activities. Set up app to connect to Firebase project.

---
+++ app/src/main/java/com/loftydevelopment/helloworldquiz/LoginActvity.kt
@@ -0,0 +1,21 @@
+package com.loftydevelopment.helloworldquiz
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActvity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_actvity)
+    }
+
+    fun loginListener(view: View){
+
+    }
+
+    fun signUpListener(view: View){
+
+    }
+}
---
+++ app/src/main/java/com/loftydevelopment/helloworldquiz/SignUpActivity.kt
@@ -0,0 +1,21 @@
+package com.loftydevelopment.helloworldquiz
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+
+    fun loginListener(view: View){
+
+    }
+
+    fun signUpListener(view: View){
+
+    }
+}
