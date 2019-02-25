3486e8fa0bd3834a6c0bf3633741cc89dc5fd4b0
Connor Loftus <connorlof@gmail.com>
2019-02-25 12:51:34 -0500

    Updated options activity layout.

--- app/src/main/java/com/loftydevelopment/helloworldquiz/OptionsActivity.kt
+++ app/src/main/java/com/loftydevelopment/helloworldquiz/OptionsActivity.kt
@@ -1,12 +1,32 @@
 package com.loftydevelopment.helloworldquiz
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class OptionsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_options)
+    }
+
+    fun saveListener(view: View){
+
+        //TODO save account setting in database
+
+        //TODO save application settings in shared pref
+
+        finish()
+        startActivity(Intent(this, MainActivity::class.java))
+
+    }
+
+    fun cancelListener(view: View){
+
+        finish()
+        startActivity(Intent(this, MainActivity::class.java))
+
     }
 }
